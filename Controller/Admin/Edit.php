0a9eaded6d47e48f57ebe2a3d34423e96f233ef1
David Yang <daworld.ny@gmail.com>
2015-11-11 17:02:28 +0500

    Fixed a typo

--- Controller/Admin/Edit.php
+++ Controller/Admin/Edit.php
@@ -9,7 +9,7 @@
  * the license file that was distributed with this source code.
  */
 
-namespace Subscrive\Controller\Admin;
+namespace Subscribe\Controller\Admin;
 
 final class Edit extends AbstractSubcriber
 {
