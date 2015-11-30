4651a959e5c00d0b50bc4e2df764cdee8fccbd70
David Yang <daworld.ny@gmail.com>
2015-11-30 14:16:50 +0500

    Added missing contract methods

--- Service/SubscribeManagerInterface.php
+++ Service/SubscribeManagerInterface.php
@@ -13,4 +13,82 @@ namespace Subscribe\Serivice;
 
 interface SubscribeManagerInterface
 {   
+    /**
+     * Updates active states
+     * 
+     * @param array $pair
+     * @return boolean
+     */
+    public function updateActiveStates(array $pair);
+
+    /**
+     * Checks whether email is already taken
+     * 
+     * @param string $email
+     * @return boolean
+     */
+    public function emailExists($email);
+
+    /**
+     * Returns last subscriber's id
+     * 
+     * @return integer
+     */
+    public function getLastId();
+
+    /**
+     * Returns prepared pagination instance
+     * 
+     * @return \Krystal\Paginate\Paginator
+     */
+    public function getPaginator();
+
+    /**
+     * Fetch all by page
+     * 
+     * @param integer $page
+     * @param integer $itemsPerPage
+     * @return array
+     */
+    public function fetchAllByPage($page, $itemsPerPage);
+
+    /**
+     * Adds a subscriber
+     * 
+     * @param array $input
+     * @return boolean
+     */
+    public function add(array $input);
+
+    /**
+     * Updates a subscriber
+     * 
+     * @param array $input Raw input data
+     * @return boolean
+     */
+    public function update(array $input);
+
+    /**
+     * Fetches a record by its id
+     * 
+     * @param string $id
+     * @return array
+     */
+    public function fetchById($id);
+
+    /**
+     * Deletes a record by its associated id
+     * 
+     * @param string $id
+     * @return boolean
+     */
+    public function deleteById($id);
+
+    /**
+     * Removes subscribers by their associated ids
+     * 
+     * @param array $ids
+     * @return boolean
+     */
+    public function deleteByIds(array $ids);
 }
