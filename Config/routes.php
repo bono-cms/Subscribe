4f72fe0b0a7c5964c444ca23996288bcba00f835
David Yang <daworld.ny@gmail.com>
2016-05-07 22:21:07 +0500

    Replaced static admin segment with its placeholder

--- Config/routes.php
+++ Config/routes.php
@@ -10,39 +10,39 @@
  */
 
 return array(
-    '/admin/module/subscribe' => array(
+    '/%s/module/subscribe' => array(
         'controller' => 'Admin:Subscriber@gridAction'
     ),
     
-    '/admin/module/subscribe/page/(:var)' => array(
+    '/%s/module/subscribe/page/(:var)' => array(
         'controller' => 'Admin:Subscriber@gridAction'
     ),
     
-    '/admin/module/subscribe/tweak' => array(
+    '/%s/module/subscribe/tweak' => array(
         'controller' => 'Admin:Subscriber@tweakAction'
     ),
     
-    '/admin/module/subscribe/add' => array(
+    '/%s/module/subscribe/add' => array(
         'controller' => 'Admin:Subscriber@addAction'
     ),
 
-    '/admin/module/subscribe/edit/(:var)' => array(
+    '/%s/module/subscribe/edit/(:var)' => array(
         'controller' => 'Admin:Subscriber@editAction'
     ),
     
-    '/admin/module/subscribe/save' => array(
+    '/%s/module/subscribe/save' => array(
         'controller' => 'Admin:Subscriber@saveAction'
     ),
     
-    '/admin/module/subscribe/send' => array(
+    '/%s/module/subscribe/send' => array(
         'controller' => 'Admin:Send@indexAction'
     ),
 
-    '/admin/module/subscribe/send.ajax' => array(
+    '/%s/module/subscribe/send.ajax' => array(
         'controller' => 'Admin:Send@sendAction'
     ),
     
-    '/admin/module/subscribe/delete' => array(
+    '/%s/module/subscribe/delete/(:var)' => array(
         'controller' => 'Admin:Subscriber@deleteAction'
     )
 );
