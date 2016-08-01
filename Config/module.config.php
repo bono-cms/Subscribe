c213987ac7477c53e8e7a52f22752610197ba472
David Yang <daworld.ny@gmail.com>
2016-08-01 22:29:57 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Subscribe',
-        'route' => 'Subscribe:Admin:Subscriber@gridAction',
-        'icon'  => 'fa fa-envelope-o fa-5x',
-        'order' => 1,
-        'description' => 'Subscribe module allows you to collect subscribers and sent mails to them'
-    )
+ 'name'  => 'Subscribe',
+ 'route' => 'Subscribe:Admin:Subscriber@gridAction',
+ 'icon'  => 'fa fa-envelope-o fa-5x',
+ 'order' => 1,
+ 'description' => 'Subscribe module allows you to collect subscribers and sent mails to them'
 );
