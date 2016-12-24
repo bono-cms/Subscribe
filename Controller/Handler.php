b7a9642e34220e0859ab5a086aceccc7baae0457
David Yang <daworld.ny@gmail.com>
2016-12-25 00:12:35 +0500

    Added site handler

---
+++ Controller/Handler.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Subscribe\Controller;
+
+use Site\Controller\AbstractController;
+use Krystal\Validate\Pattern;
+
+final class Handler extends AbstractController
+{
+    /**
+     * Renders the message
+     * 
+     * @param string $template
+     * @param array $vars
+     * @return string
+     */
+    private function createMessage($template, array $vars)
+    {
+        // Configure view. Override inherited configuration
+        $this->view->setModule('Subscribe')
+                   ->setTheme('site')
+                   ->disableLayout();
+
+        return $this->view->render($template, $vars);
+    }
+
+    /**
+     * Confirms an email
+     * 
+     * @param string $key
+     * @return string
+     */
+    public function confirmAction($key)
+    {
+        $service = $this->getModuleService('subscribeManager');
+
+        return $this->createMessage('confirmation', array(
+            'success' => $service->confirmUser($key),
+            'message' => 'Thank you! You have successfully confirmed your email'
+        ));
+    }
+
+    /**
+     * Subsribes a user by their email addresss
+     * 
+     * @return string
+     */
+    public function subcribeAction()
+    {
+        if ($this->request->isPost()) {
+            $data = $this->request->getPost();
+
+            // Create form validator
+            $formValudator = $this->createValidator(array(
+                'input' => array(
+                    'source' => $data,
+                    'definition' => array(
+                        'email' => new Pattern\Email(),
+                        'name' => new Pattern\Name()
+                    )
+                )
+            ));
+
+            if ($formValudator->isValid()) {
+                $service = $this->getModuleService('subscribeManager');
+
+                // Prepare common variables
+                $key = $service->subscribe($data['name'], $data['email']);
+                $url = $this->request->getBaseUrl();
+                $subject = $this->translator->translate('Confirm your email');
+
+                $message = $this->createMessage('subscribe', array(
+                    'name' => $data['name'],
+                    'site' => $url,
+                    'link' => $url.$this->createUrl('Subscribe:Handler@confirmAction', array($key))
+                ));
+
+                // Send e-mail notification now
+                $mailer = $this->getService('Cms', 'mailer');
+                $mailer->sendTo($data['email'], $subject, $message);
+
+                // Successfullt sent a request to news letters
+                return '1';
+
+            } else {
+                return $formValudator->getErrors();
+            }
+        }
+    }
+
+    /**
+     * Unsubsribes a user by their email addresss
+     * 
+     * @param string $key
+     * @return string
+     */
+    public function unsubscribeAction($key)
+    {
+        $service = $this->getModuleService('subscribeManager');
+
+        return $this->createMessage('confirmation', array(
+            'success' => $service->unsubscribe($key),
+            'message' => 'You have successfully unsubscribed from our mailing list'
+        ));
+    }
+}
