e6c9ebc45fd539fb9ebcabb3343d31ae2155eae8
David Yang <daworld.ny@gmail.com>
2016-12-30 21:37:58 +0500

    Changed description

--- Config/module.config.php
+++ Config/module.config.php
@@ -6,5 +6,5 @@ return array(
  'route' => 'Subscribe:Admin:Subscriber@gridAction',
  'icon'  => 'fa fa-envelope-o fa-5x',
  'order' => 1,
- 'description' => 'Subscribe module allows you to collect subscribers and sent mails to them'
+ 'description' => 'Subscribe module allows you to collect subscribers and send them news-lettters'
 );
