4d6b65f6e9ef73dbc9ee5590ab75186e77111906
dave-yang <daworld.ny@gmail.com>
2019-03-08 20:12:48 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,24 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'name'  => 'Subscribe',
- 'caption'  => 'Subscribe',
- 'route' => 'Subscribe:Admin:Subscriber@gridAction',
- 'order' => 1,
- 'description' => 'Subscribe module allows you to collect subscribers and send them news-lettters'
+    'name'  => 'Subscribe',
+    'description' => 'Subscribe module allows you to collect subscribers and send them news-lettters',
+    'menu' => array(
+        'name' => 'Subscribe',
+        'icon' => 'fas fa-envelope',
+        'items' => array(
+            array(
+                'route' => 'Subscribe:Admin:Subscriber@gridAction',
+                'name' => 'View all subscribers'
+            ),
+            array(
+                'route' => 'Subscribe:Admin:Send@indexAction',
+                'name' => 'New bulk-sending'
+            )
+        )
+    )
 );
